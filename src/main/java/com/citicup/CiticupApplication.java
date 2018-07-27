bf3751da6bd4d4d3df63ba5c06d141d0c42bdf1e
161158053 <30664307+Cauchy-NY@users.noreply.github.com>
2018-07-27 22:28:53 +0800

    自动生成dao层测试

--- src/main/java/com/citicup/CiticupApplication.java
+++ src/main/java/com/citicup/CiticupApplication.java
@@ -1,10 +1,13 @@
 package com.citicup;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CiticupApplication {
+@MapperScan("com.citicup.dao")
+public class CiticupApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiticupApplication.class, args);
--- src/main/java/com/citicup/controller/UserController.java
+++ src/main/java/com/citicup/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.citicup.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/testboot")
 public class UserController {
+
+//    @RequestMapping("getuser")
+//    public User getUser() {
+//
+//    }
 }
