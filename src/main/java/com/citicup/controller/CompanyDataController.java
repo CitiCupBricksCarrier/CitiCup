c6875468bbe4860ee9ed4b4431bdd1f8c6ad6418
zyb <zyb250250@qq.com>
2018-08-02 22:36:53 +0800

    新增接口

---
+++ src/main/java/com/citicup/controller/CompanyDataController.java
@@ -0,0 +1,47 @@
+package com.citicup.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.citicup.dao.CompanyBasicInformationMapper;
+import com.citicup.dao.CompanyClassificationMapper;
+import com.citicup.model.ChinesePassengerCarSalesDividedByBrand;
+import com.citicup.model.CompanyBasicInformation;
+import com.citicup.model.CompanyClassification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/generalInfo")
+public class CompanyDataController {
+
+    @Autowired
+    private CompanyBasicInformationMapper companyBasicInformationMapper;
+    @Autowired
+    private CompanyClassificationMapper companyClassificationMapper;
+
+    /**
+     * 公司基本信息
+     * @return
+     */
+    @RequestMapping("/companyDetail")
+    public String selectcompanyDetail() {
+
+        List<CompanyBasicInformation> list = companyBasicInformationMapper.getAll();
+        return JSONObject.toJSONString(list);
+    }
+
+    /**
+     * 公司分类
+     * @return
+     */
+    @RequestMapping("/companyList")
+    public String selectcompanyList() {
+
+        List<CompanyClassification> list = companyClassificationMapper.getAll();
+        return JSONObject.toJSONString(list);
+    }
+}
