e4936bd575185a320f8c5b92e7b659dac26dce23
zyb <zyb250250@qq.com>
2018-08-31 18:46:14 +0800

    fix 跨域session不一致

---
+++ src/main/java/com/citicup/CORSFilter.java
@@ -0,0 +1,31 @@
+package com.citicup;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        HttpServletRequest request=(HttpServletRequest)servletRequest;
+        res.setContentType("textml;charset=UTF-8");
+        res.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        res.setHeader("Access-Control-Max-Age", "0");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("XDomainRequestAllowed","1");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
