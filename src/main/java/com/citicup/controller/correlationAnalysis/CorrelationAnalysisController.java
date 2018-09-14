ac5f6ed71b115fe5c7b8a09fa0eb14d7f18175a2
161250038 <161250038@smail.nju.edu.cn>
2018-09-14 15:52:48 +0800

    更新分析方法

--- src/main/java/com/citicup/controller/correlationAnalysis/CorrelationAnalysisController.java
+++ src/main/java/com/citicup/controller/correlationAnalysis/CorrelationAnalysisController.java
@@ -17,45 +17,26 @@ import java.util.*;
 @EnableAutoConfiguration
 @RequestMapping("/CorrelationAnalysis")
 public class CorrelationAnalysisController {
-    @Autowired
+
     private InventoryTurnoverMapper inventoryTurnoverMapper;
-    @Autowired
     private Quick_RatioMapper quick_ratioMapper;
-    @Autowired
     private TurnoverOfTotalAssetsMapper turnoverOfTotalAssetsMapper;
-    @Autowired
     private ROE_changeMapper roe_changeMapper;
-    @Autowired
     private GPOA_changeMapper gpoa_changeMapper;
-    @Autowired
     private ProfitGrowthRateMapper profitGrowthRateMapper;
-    @Autowired
     private ConsistenceExpectationMapper consistenceExpectationMapper;
-    @Autowired
     private ConsistenceExpectationPredictProfitMapper consistenceExpectationPredictProfitMapper;
-    @Autowired
     private DealersNumChange_1MMapper dealersNumChange_1MMapper;
-    @Autowired
     private DividendRateMapper dividendRateMapper;
-    @Autowired
     private MarketSellingRateRecMapper marketSellingRateRecMapper;
-    @Autowired
     private PE_RatioRecMapper pe_ratioRecMapper;
-    @Autowired
     private TurnoverStd_6DMapper turnoverStd_6DMapper;
-    @Autowired
     private TurnoverMean_6DMapper turnoverMean_6DMapper;
-    @Autowired
     private TenoreRatio_20DMapper tenoreRatio_20DMapper;
-    @Autowired
     private TenoreRatio_60DMapper tenoreRatio_60DMapper;
-    @Autowired
     private CostProfitMarginCumuMapper costProfitMarginCumuMapper;
-    @Autowired
     private IncometaxProfitPercentMapper incometaxProfitPercentMapper;
-    @Autowired
     private AnalysisIndexVisitorMapper analysisIndexVisitorMapper;
-    @Autowired
     private stockEPSMapper stockEPSMapper;
 
     private static final String[] indexsName = new String[]{"ConsistenceExpectation", "ConsistenceExpectationPredictProfit",
@@ -65,32 +46,8 @@ public class CorrelationAnalysisController {
     private static final int dayGap = 2;
 
     /**
-     * 单个指标当天增加一次访问量
-     * @return
-     */
-    private void increaseIndexClicks(String analysisIndex){
-        Date today = new Date(); // 获取当日日期
-        AnalysisIndexVisitorKey key = new AnalysisIndexVisitorKey();
-        key.setAnalysisindex(analysisIndex);
-        key.setDate(today);
-        if(null == analysisIndexVisitorMapper.selectByPrimaryKey(key)) {
-            AnalysisIndexVisitor analysisIndexVisitor = new AnalysisIndexVisitor();
-            analysisIndexVisitor.setAnalysisindex(analysisIndex);
-            analysisIndexVisitor.setDate(today);
-            analysisIndexVisitor.setVistornumber(1);
-            analysisIndexVisitorMapper.insert(analysisIndexVisitor);
-        }
-        else {
-            AnalysisIndexVisitor analysisIndexVisitor = analysisIndexVisitorMapper.selectByPrimaryKey(key);
-            int newValue = analysisIndexVisitor.getVistornumber() + 1;
-            analysisIndexVisitor.setVistornumber(newValue);
-            analysisIndexVisitorMapper.updateByPrimaryKeySelective(analysisIndexVisitor);
-        }
-    }
-
-    /**
-     * 各指标最近3天的选择人数（所有指标一起）
-     * @return
+     * 获取各指标最近3天的选择人数（所有指标一起）
+     * @return 各指标最近3天的选择人数
      */
     @RequestMapping("/IndexClicks")
     public String selectIndexClicks() {
@@ -118,97 +75,48 @@ public class CorrelationAnalysisController {
         return JSONObject.toJSONString(json);
     }
 
+    /**
+     * 单个指标当天增加一次访问量
+     *
+     */
+    private void increaseIndexClicks(String analysisIndex){
+        Date today = new Date(); // 获取当日日期
+        AnalysisIndexVisitorKey key = new AnalysisIndexVisitorKey();
+        key.setAnalysisindex(analysisIndex);
+        key.setDate(today);
+        if(null == analysisIndexVisitorMapper.selectByPrimaryKey(key)) {
+            AnalysisIndexVisitor analysisIndexVisitor = new AnalysisIndexVisitor();
+            analysisIndexVisitor.setAnalysisindex(analysisIndex);
+            analysisIndexVisitor.setDate(today);
+            analysisIndexVisitor.setVistornumber(1);
+            analysisIndexVisitorMapper.insert(analysisIndexVisitor);
+        }
+        else {
+            AnalysisIndexVisitor analysisIndexVisitor = analysisIndexVisitorMapper.selectByPrimaryKey(key);
+            int newValue = analysisIndexVisitor.getVistornumber() + 1;
+            analysisIndexVisitor.setVistornumber(newValue);
+            analysisIndexVisitorMapper.updateByPrimaryKeySelective(analysisIndexVisitor);
+        }
+    }
+
+    /**
+     * 指标对行业的分析
+     * @return 分析结果
+     */
     @RequestMapping("/AnalysisIndustry")
-    public String selectAnalysisIndustry(String index, String analysisMethod) {
-        return null;
-    }
-
-    /**
-     * 标准化
-     * @return
-     */
-    private List<String> standardize(List<Index> list) {
-        List<String> series = new ArrayList<>();
-        for(int i = 0; i<list.size(); i++){
-            series.add(list.get(i).getStkcd());
-        }
-
-        for(int i = 0; i<list.size()-1; i++) {
-            for(int j = 0; j<list.size()-1; j++) {
-                double a = Double.parseDouble(list.get(j).getValue());
-                double b = Double.parseDouble(list.get(j+1).getValue());
-                if(a > b) {
-                    String tmp = series.get(j);
-                    series.set(j, series.get(j+1));
-                    series.set(j+1, tmp);
-                }
-            }
-        }
-
-        return series;
-    }
-
-    /**
-     * 计算 某一周期 IC值
-     * @return
-     */
-    private double getIC(List<Double> list) {
-        int size = list.size() / 10; //表示行业股票池中，10%股票的数目
-        List<Double> top = new ArrayList<>();
-        List<Double> bottom = new ArrayList<>();
-        for(int i = 0, j = list.size() - size; i<size && j<list.size(); i++, j++) {
-            top.add(list.get(i));
-            bottom.add(list.get(j));
-        }
-
-        double a = 0.0, b = 0.0, c = 0.0;
-        double aveOfTop = 0.0, aveOfBot = 0.0, stdOfTop = 0.0, stdOfBot = 0.0;
-        for(int i = 0; i<size; i++) {
-            a += top.get(i) * bottom.get(i);
-            b += top.get(i);
-            c += bottom.get(i);
-        }
-        aveOfTop = b / size;
-        aveOfBot = c / size;
-        for(int i = 0; i<size; i++) {
-            stdOfTop += Math.pow(top.get(i) - aveOfTop, 2);
-            stdOfBot += Math.pow(bottom.get(i) - aveOfBot, 2);
-        }
-        stdOfTop = Math.sqrt(stdOfTop / size);
-        stdOfBot = Math.sqrt(stdOfBot / size);
-        double result = (a - (b*c)/size) / (stdOfTop * stdOfBot);
-
-        return result;
-    }
-
-    /**
-     * 计算 IC-mean 指标
-     * @return
-     */
-    @RequestMapping("/IC_Mean")
-    public String getIC_Mean(@RequestParam String index) {
+    public String selectAnalysisIndustry(@RequestParam String index, @RequestParam String analysisMethod) {
         increaseIndexClicks(index);
-        List<stockEPS> stockEPSList = stockEPSMapper.getAll();
-        int periodNumber = 0; // 周期数
-        for(int i = 0; i < stockEPSList.size(); i++ ) { //计算非空有效数据的周期数（按季度为单位）
-            if(stockEPSList.get(i).getValue() != null || !stockEPSList.get(i).getValue().equals("")) {
-                periodNumber++;
-            }
-            else {
-                stockEPSList.remove(i);
-                i--;
-            }
-        }
+        List<Index> list = new ArrayList<>();
 
-        switch(index){
+        switch (index){
             case "安全性-存货周转率":
-                List<InventoryTurnover> list = inventoryTurnoverMapper.getAll();
-                for(int i = 0; i < list.size(); i++) {
-                    if(list.get(i).getValue() == null || list.get(i).getValue().equals("")) {
-                        list.remove(i);
-                        i--;
+                List<InventoryTurnover> inv = inventoryTurnoverMapper.getAll();
+                for (InventoryTurnover anInv : inv) {
+                    if (anInv.getValue() != null && !anInv.getValue().equals("")) {
+                        list.add(new Index(anInv.getStkcd(), anInv.getDate(), anInv.getValue()));
                     }
                 }
+
                 break;
             case "安全性类-速动比率":
                 break;
@@ -248,20 +156,236 @@ public class CorrelationAnalysisController {
                 return "查找错误";
         }
 
+        String reply = "";
+        double result = 0.0;
+        switch (analysisMethod) {
+            case "IC-mean":
+                result = getIC_Mean(list);
+                if(result > 0.1) {
+                    reply = "您所选择的指标和行业股票的收益率之间有较强的相关性。";
+                }
+                else if(0.1 >=result && result > 0.05) {
+                    reply = "您所选择的指标和行业股票的收益率之间有相关性。";
+                }
+                else if(0.05 >= result && result >0.02) {
+                    reply = "您所选择的指标和行业股票的收益率之间相关性较弱。";
+                }
+                else {
+                    reply = "您所选择的指标和行业股票的收益率之间几乎没有相关性。";
+                }
+                break;
+            case "IC-IR":
+                result = getIC_IR(list);
+                if(result > 0.7) {
+                    reply = "您所选择的指标和行业股票的收益率之间有较强的相关性，且相关性稳定。";
+                }
+                else if(0.7 >=result && result > 0.5) {
+                    reply = "您所选择的指标和行业股票的收益率之间有相关性，且相关性较稳定。";
+                }
+                else if(0.5 >= result && result >0.2) {
+                    reply = "您所选择的指标和行业股票的收益率之间相关性较弱，且相关性不太稳定。";
+                }
+                else {
+                    reply = "您所选择的指标和行业股票的收益率之间几乎没有相关性。";
+                }
+                break;
+            case "IC-T":
+                result = getIC_T(list);
+                if(result > 0.7) {
+                    reply = "您所选择的指标和行业股票的收益率之间几乎没有相关性。";
+                }
+                else if(0.7 >=result && result > 0.5) {
+                    reply = "您所选择的指标和行业股票的收益率之间相关性较不显著。";
+                }
+                else if(0.5 >= result && result >0.2) {
+                    reply = "您所选择的指标和行业股票的收益率之间有相关性。";
+                }
+                else {
+                    reply = "您所选择的指标和行业股票的收益率之间有较显著的相关性。";
+                }
+                break;
+            case "一元线性回归":
 
+                break;
+            case "多空收益":
+                result = getLong_Short_Earnings(list);
+                reply = "多空收益可用来探究您选择的因子对整个行业股票收益率的影响；你所选择的因子对应的多空收益为："+result;
+                break;
+            default:
+                return "无此方法";
+        }
 
-        return null;
+        return JSONObject.toJSONString(reply);
     }
 
     /**
-     * 计算 IC-std 指标
-     * @return
+     * 标准化
+     * @return 索引为排名，内容为股票代码
      */
-    @RequestMapping("/IC_Mean")
-    public String getIC_std(@RequestParam String index) {
-        return "hello";
+    private List<String> standardize(List<Index> list) {
+        List<String> series = new ArrayList<>();
+        for (Index aList : list) {
+            series.add(aList.getStkcd());
+        }
+
+        for(int i = 0; i<list.size()-1; i++) {
+            for(int j = 0; j<list.size()-1; j++) {
+                double a = Double.parseDouble(list.get(j).getValue());
+                double b = Double.parseDouble(list.get(j+1).getValue());
+                if(a > b) {
+                    String tmp = series.get(j);
+                    series.set(j, series.get(j+1));
+                    series.set(j+1, tmp);
+                }
+            }
+        }
+
+        return series;
     }
 
+    /**
+     * 计算 某一周期 IC值
+     * @return IC值
+     */
+    private double getIC(List<Double> list) {
+        int size = list.size() / 10; //表示行业股票池中，10%股票的数目
+        List<Double> top = new ArrayList<>();
+        List<Double> bottom = new ArrayList<>();
+        for(int i = 0, j = list.size() - size; i<size && j<list.size(); i++, j++) {
+            top.add(list.get(i));
+            bottom.add(list.get(j));
+        }
 
+        double a = 0.0, b = 0.0, c = 0.0;
+        double aveOfTop = 0.0, aveOfBot = 0.0, stdOfTop = 0.0, stdOfBot = 0.0;
+        for(int i = 0; i<size; i++) {
+            a += top.get(i) * bottom.get(i);
+            b += top.get(i);
+            c += bottom.get(i);
+        }
+        aveOfTop = b / size;
+        aveOfBot = c / size;
+        for(int i = 0; i<size; i++) {
+            stdOfTop += Math.pow(top.get(i) - aveOfTop, 2);
+            stdOfBot += Math.pow(bottom.get(i) - aveOfBot, 2);
+        }
+        stdOfTop = Math.sqrt(stdOfTop / size);
+        stdOfBot = Math.sqrt(stdOfBot / size);
+        if(stdOfTop * stdOfBot == 0) { System.out.println("除零错误"); System.exit(0);}
+        double result = (a - (b*c)/size) / (stdOfTop * stdOfBot);
+
+        return result;
+    }
+
+    /**
+     * 计算 IC-mean 指标
+     * @return IC-mean 指标
+     */
+    private double getIC_Mean(List<Index> list) {
+        List<stockEPS> stockEPSList = stockEPSMapper.getAll();
+        Map<String, List<Index>> epsPerQuarter = new LinkedHashMap<>();
+        double sumOfIC = 0.0;
+
+        for(stockEPS s : stockEPSList){
+            String date = s.getDate();
+            Index index = stockEPS2Index(s);
+            if(epsPerQuarter.containsKey(date)) {
+                epsPerQuarter.get(date).add(index);
+            }
+            else {
+                List<Index> listOfIndexes = new ArrayList<>();
+                listOfIndexes.add(index);
+                epsPerQuarter.put(date, listOfIndexes);
+            }
+        }
+
+        if(epsPerQuarter.size() <= 0) { System.out.println("股票收益内容为空"); System.exit(0);}
+
+        for(String quarter : epsPerQuarter.keySet()) {
+            List<Index> tmp = new ArrayList<>();
+            for(Index idx : list) {
+                String[] date = idx.getDate().split("/");
+                if(quarter.substring(0,4).equals(date[0])){
+                    switch (quarter.substring(4,6)){
+                        case "01":
+                            if(date[1].equals("2")){
+                                tmp.add(idx);
+                            }
+                            break;
+                        case "02":
+                            if(date[1].equals("5")){
+                                tmp.add(idx);
+                            }
+                            break;
+                        case "03":
+                            if(date[1].equals("8")){
+                                tmp.add(idx);
+                            }
+                            break;
+                        case "04":
+                            if(date[1].equals("11")){
+                                tmp.add(idx);
+                            }
+                            break;
+                    }
+                }
+            }
+            List<String> standardizedIndexes = standardize(tmp);
+            List<String> standardizedEps = standardize(epsPerQuarter.get(quarter));
+            List<Double> series = new ArrayList<>();
+            for (String standardizedIndex : standardizedIndexes) {
+                for (int j = 0; j < standardizedEps.size(); j++) {
+                    if (standardizedIndex.equals(standardizedEps.get(j))) {
+                        series.add((double) j);
+                    }
+                }
+            }
+            sumOfIC += getIC(series);
+        }
+        double IC_Mean = sumOfIC / epsPerQuarter.size();
+
+        return IC_Mean;
+    }
+
+    private Index stockEPS2Index(stockEPS s){
+         return  new Index(s.getStkcd(),s.getDate(),s.getValue());
+    }
+
+    /**
+     * 计算 IC-IR 指标
+     * @return IC-IR 指标
+     */
+    private double getIC_IR(List<Index> list) {
+
+        return 0.0;
+    }
+
+    /**
+     * 计算 IC-T 指标
+     * @return IC-T 指标
+     */
+    private double getIC_T(List<Index> list) {
+        return 0.0;
+    }
+
+    /**
+     * 计算 多空收益 指标
+     * @return 多空收益 指标
+     */
+    private double getLong_Short_Earnings(List<Index> list) {
+        return 0.0;
+    }
+
+    /**
+     * 计算 一元线性回归
+     * @return 一元线性回归
+     */
+    private String getLinearRegression(List<Index> list) {
+        return null;
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println("hello");
+//    }
 
 }
