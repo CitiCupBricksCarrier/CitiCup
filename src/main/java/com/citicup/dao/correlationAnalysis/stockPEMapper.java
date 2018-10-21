ed6a318dc68f406fabfaf053403f88e5e64b34d8
LAPTOP-6OV21V4L\lenovo <987801208@qq.com>
2018-10-21 22:34:57 +0800

    stockpe

--- src/main/java/com/citicup/dao/correlationAnalysis/stockPEMapper.java
+++ src/main/java/com/citicup/dao/correlationAnalysis/stockPEMapper.java
@@ -1,9 +1,6 @@
 package com.citicup.dao.correlationAnalysis;
 
 import com.citicup.model.correlationAnalysis.stockPE;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 public interface stockPEMapper {
     /**
@@ -53,7 +50,4 @@ public interface stockPEMapper {
      * @mbggenerated
      */
     int updateByPrimaryKey(stockPE record);
-
-    @Select("SELECT * FROM citicup.stockpe")
-    List<stockPE> getAll();
 }
