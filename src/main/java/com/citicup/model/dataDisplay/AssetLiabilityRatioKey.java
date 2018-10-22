5da0062cb79ebed5e7127503c51504176b3d3ec8
zyb <zyb250250@qq.com>
2018-10-22 22:31:39 +0800

    add 竞争者对比接口

--- src/main/java/com/citicup/model/dataDisplay/AssetLiabilityRatioKey.java
+++ src/main/java/com/citicup/model/dataDisplay/AssetLiabilityRatioKey.java
@@ -35,7 +35,7 @@ public class AssetLiabilityRatioKey implements Serializable {
      *
      * @mbggenerated
      */
-    public String get﻿stkcd() {
+    public String getStkcd() {
         return stkcd;
     }
 
@@ -47,7 +47,7 @@ public class AssetLiabilityRatioKey implements Serializable {
      *
      * @mbggenerated
      */
-    public void set﻿stkcd(String stkcd) {
+    public void setStkcd(String stkcd) {
         this.stkcd = stkcd == null ? null : stkcd.trim();
     }
 
@@ -93,7 +93,7 @@ public class AssetLiabilityRatioKey implements Serializable {
             return false;
         }
         AssetLiabilityRatioKey other = (AssetLiabilityRatioKey) that;
-        return (this.get﻿stkcd() == null ? other.get﻿stkcd() == null : this.get﻿stkcd().equals(other.get﻿stkcd()))
+        return (this.getStkcd() == null ? other.getStkcd() == null : this.getStkcd().equals(other.getStkcd()))
             && (this.getAcceper() == null ? other.getAcceper() == null : this.getAcceper().equals(other.getAcceper()));
     }
 
@@ -107,7 +107,7 @@ public class AssetLiabilityRatioKey implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((get﻿stkcd() == null) ? 0 : get﻿stkcd().hashCode());
+        result = prime * result + ((getStkcd() == null) ? 0 : getStkcd().hashCode());
         result = prime * result + ((getAcceper() == null) ? 0 : getAcceper().hashCode());
         return result;
     }
