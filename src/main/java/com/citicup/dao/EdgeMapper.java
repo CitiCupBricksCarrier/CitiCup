16f9dd3627860791713df5e1d05dac7615350fc8
161158053 <30664307+Cauchy-NY@users.noreply.github.com>
2018-10-27 23:05:30 +0800

    debug: edge批量存储bug

--- src/main/java/com/citicup/dao/EdgeMapper.java
+++ src/main/java/com/citicup/dao/EdgeMapper.java
@@ -113,9 +113,10 @@ public interface EdgeMapper {
     @Select({"SELECT * FROM edge WHERE graphid = #{graphId, jdbcType=VARCHAR}"})
     List<Edge> getAllById(String graphId);
 
-    @Insert({"<script> <foreach collection=\"list\" item=\"item\" index=\"index\"  separator=\",\"> " +
-            "INSERT INTO edge(stkcda, stkcdb, graphid, status, propagateproba, propagateprobb, fund, id, begin, end) VALUES " +
-            "(#{item.stkcda},#{item.stkcdb},#{item.graphid},#{item.status},#{item.propagateproba},#{item.propagateprobb},#{item.fund},#{item.id},#{item.begin},#{item.end})" +
+    @Insert({"<script> INSERT INTO edge(stkcda, stkcdb, graphid, status, propagateproba, propagateprobb, fund, id, begin, end) " +
+            "VALUES " +
+            "<foreach collection=\"list\" item=\"item\" index=\"index\"  separator=\",\"> "+
+            "(#{item.stkcda},#{item.stkcdb},#{item.graphid},#{item.status},#{item.propagateproba},#{item.propagateprobb},#{item.fund},#{item.id},#{item.begin},#{item.end})"+
             "</foreach> </script>"})
     int saveEdgeList(@Param("list") List<Edge> list);
 
